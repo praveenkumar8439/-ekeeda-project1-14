67c3af2e51996a8535869de0cec4f7dbace70f65
praveenkumar8439 <126686371+praveenkumar8439@users.noreply.github.com>
2023-04-04 20:39:14 +0530

    Add files via upload

---
+++ my portfolio/java.js
@@ -0,0 +1,11 @@
+
+function myFunction(){
+    var x = document.getElementById("menu1");    
+    if(x.style.display === 'none'){
+        x.style.display==='block';
+    console.log(myFunction);
+    }else{
+        x.style.display==="none";
+    }
+    // x.style.display==="none"
+}
